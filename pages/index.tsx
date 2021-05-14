40d041e56a009cfecd81906e59eca98e90596865
Viviane Vieira <viviane.kodama@gmail.com>
2021-05-14 15:24:43 -0700

    Created a separate function to check if user is logged in.

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import { parseCookies } from 'nookies';
 import { AuthContext } from '../contexts/AuthContext';
 
 import styles from '../styles/Home.module.css'
+import { withSSRGuest } from '../utils/withSSRGuest';
 
 export default function Home() {
   const [email, setEmail] = useState('');
@@ -34,19 +35,8 @@ export default function Home() {
 }
 
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const cookies = parseCookies(ctx);
-
-  if (cookies['nextauth.token']) {
-    return {
-      redirect: {
-        destination: '/dashboard',
-        permanent: false
-      }
-    }
-  }
-
+export const getServerSideProps = withSSRGuest(async (ctx) => {
   return {
     props: {}
   }
-}
+});
