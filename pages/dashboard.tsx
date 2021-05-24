b9158b4da124a5edb655f432f0ffd46e04c8a1c6
Viviane Vieira <viviane.kodama@gmail.com>
2021-05-24 11:53:32 -0700

    Redirecting user from server side.

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -22,7 +22,7 @@ export const getServerSideProps = withSSRAuth(async (ctx) => {
   const apiClient = setupAPIClient(ctx);
   const response = await apiClient.get('/me');
 
-  console.log(response.data);
+  console.log(response.data)
 
   return {
     props: {}
